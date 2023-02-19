5d8728c8e067431e07fff0d69a31def68c5e154c
David Casas <casas181@yahoo.com>
2023-02-18 22:44:01 -0800

    Added route function to create account screen and cleaned code

--- lib/createaccount.dart
+++ lib/createaccount.dart
@@ -1,54 +1,33 @@
 import 'package:flutter/material.dart';
-import './main.dart';
+import './login.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+class CreateAccount extends StatelessWidget {
+  const CreateAccount({super.key});
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  static const routeName = '/create-account';
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Tesscuro',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const CreateAccount(title: 'Create Account'),
-    );
+  //Login Page Route
+  void loginRoute(BuildContext ctx) {
+    Navigator.of(ctx).pushNamed(LoginPage.routeName);
   }
-}
 
-class CreateAccount extends StatefulWidget {
-  const CreateAccount({super.key, required this.title});
-  final String title;
-
-  @override
-  State<CreateAccount> createState() => _CreateAccountState();
-}
-
-class _CreateAccountState extends State<CreateAccount> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Create Account'),
       ),
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            /*Padding(
-              padding: EdgeInsets.all(25),
-              child: Center(
-                child: SizedBox(
-                  width: 200,
-                  height: 150,
-                ),
-              ),
-            ),*/
+            
+            //Username Field
             const Padding(
-              padding: EdgeInsets.only(top: 40, left: 10, right: 10),
+              padding: EdgeInsets.only(
+                top: 40,
+                left: 10,
+                right: 10,
+              ),
               child: TextField(
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
@@ -57,8 +36,14 @@ class _CreateAccountState extends State<CreateAccount> {
                 ),
               ),
             ),
+
+            //Email Field
             const Padding(
-              padding: EdgeInsets.only(top: 40, left: 10, right: 10),
+              padding: EdgeInsets.only(
+                top: 40,
+                left: 10,
+                right: 10,
+              ),
               child: TextField(
                 obscureText: true,
                 decoration: InputDecoration(
@@ -68,8 +53,14 @@ class _CreateAccountState extends State<CreateAccount> {
                 ),
               ),
             ),
+
+            //Password Field
             const Padding(
-              padding: EdgeInsets.only(top: 40, left: 10, right: 10),
+              padding: EdgeInsets.only(
+                top: 40,
+                left: 10,
+                right: 10,
+              ),
               child: TextField(
                 obscureText: true,
                 decoration: InputDecoration(
@@ -79,8 +70,15 @@ class _CreateAccountState extends State<CreateAccount> {
                 ),
               ),
             ),
+
+            //Password Validation
             const Padding(
-              padding: EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 50),
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 10,
+                right: 10,
+                bottom: 50,
+              ),
               child: TextField(
                 obscureText: true,
                 decoration: InputDecoration(
@@ -90,21 +88,19 @@ class _CreateAccountState extends State<CreateAccount> {
                 ),
               ),
             ),
+
+            //Submit Button
             SizedBox(
               height: 60,
               width: 200,
               child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const LoginPage(title: 'Login Page'),
-                    ),
-                  );
-                },
+                onPressed: () => loginRoute(context),
                 child: const Text(
                   'Submit',
-                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
                 ),
               ),
             ),
