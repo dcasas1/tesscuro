0ad19b77fac2afe4b1c6c8ba0b624bffda1efa96
David Casas <casas181@yahoo.com>
2023-02-18 22:40:34 -0800

    Split main from login to shorten code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import './createaccount.dart';
 import './homepage.dart';
+import './login.dart';
+import './addcredentials.dart';
+import './editsettings.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -17,105 +21,15 @@ class MyApp extends StatelessWidget {
         canvasColor: Colors.white,
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(title: 'Login Page'),
-    );
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 110, bottom: 50),
-              child: Center(
-                child: SizedBox(
-                  child: Image.asset(
-                    'assets/img/tesscuro_logo.png',
-                    scale: 3,
-                  ),
-                ),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, bottom: 20),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Username',
-                  hintText: 'Enter Username',
-                ),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, bottom: 30),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'Enter password',
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 60,
-              width: 200,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const HomePage(),
-                    ),
-                  );
-                },
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: Colors.black, fontSize: 20),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: SizedBox(
-                height: 60,
-                width: 200,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const CreateAccount(
-                          title: 'CreateAccount',
-                        ),
-                      ),
-                    );
-                  },
-                  child: const Text(
-                    'Create Account',
-                    style: TextStyle(color: Colors.black, fontSize: 20),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      //home: const LoginPage(title: 'Login Page'),
+      routes: {
+        '/': (ctx) => const LoginPage(),
+        HomePage.routeName: (ctx) => const HomePage(),
+        CreateAccount.routeName: (ctx) => const CreateAccount(),
+        LoginPage.routeName: (ctx) => const LoginPage(),
+        AddCredentials.routeName:(ctx) => const AddCredentials(),
+        EditSettings.routeName:(ctx) => const EditSettings(),
+      },
     );
   }
 }
