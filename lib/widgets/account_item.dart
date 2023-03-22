31dc0dc879d6cc85a295bd204340844f6714eb8a
David Casas <casas181@yahoo.com>
2023-03-21 23:53:12 -0700

    Added swipe to dismiss functionality from app

--- lib/widgets/account_item.dart
+++ lib/widgets/account_item.dart
@@ -22,30 +22,77 @@ class _AccountViewState extends State<AccountView> {
   @override
   Widget build(BuildContext context) {
     final account = Provider.of<Accounts>(context, listen: false);
-    return ListTile(
-      title: Text(account.siteName),
-      subtitle: _passwordVisible
-          ? Text(
-              "Username: ${account.userName}\nPassword: ${account.password.replaceAll(account.password, "********")}")
-          : Text(
-              "Username: ${account.userName}\nPassword: ${account.password}"),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          IconButton(
-            icon: Icon(
-                _passwordVisible ? Icons.visibility_off : Icons.visibility),
-            onPressed: () {
-              setState(() {
-                _passwordVisible = !_passwordVisible;
-              });
-            },
+    return Dismissible(
+      key: ValueKey(account.siteUrl),
+      direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: const Text(
+              'Are you sure?',
+            ),
+            content: const Text(
+              'Do you want to remove this account?',
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+                child: const Text('No'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+                child: const Text('Yes'),
+              )
+            ],
           ),
-          IconButton(
-            icon: const Icon(Icons.edit),
-            onPressed: () {},
-          ),
-        ],
+        );
+      },
+      background: Container(
+        color: Theme.of(context).colorScheme.error,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(
+          right: 20,
+        ),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+      ),
+      child: ListTile(
+        title: Text(account.siteName),
+        subtitle: _passwordVisible
+            ? Text(
+                "Username: ${account.userName}\nPassword: ${account.password.replaceAll(account.password, "********")}")
+            : Text(
+                "Username: ${account.userName}\nPassword: ${account.password}"),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              icon: Icon(
+                  _passwordVisible ? Icons.visibility_off : Icons.visibility),
+              onPressed: () {
+                setState(() {
+                  _passwordVisible = !_passwordVisible;
+                });
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.edit),
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
