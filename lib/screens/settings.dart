12f8ba3e0fda230e2f36996e81155890a678ac78
Miguel Bueno <mbueno-nunez@csub.edu>
2023-03-29 19:18:51 -0700

    functionality for toggle button to turn screen dark and light has now been added

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -15,65 +15,102 @@ class Settings extends StatefulWidget {
 
 //generate password route
 void generateRoute(BuildContext ctx) {
-    Navigator.of(ctx).pushNamed(GeneratePassword.routeName);
-  }
+  Navigator.of(ctx).pushNamed(GeneratePassword.routeName);
+}
 
 void createRoute(BuildContext ctx) {
-   Navigator.of(ctx).pushNamed(CreateAccount.routeName);
-  }
+  Navigator.of(ctx).pushNamed(CreateAccount.routeName);
+}
 
 class _SettingsState extends State<Settings> {
   bool light = true;
-  // bool _darkModeEnabled = false;
+  bool _darkModeEnabled = false;
 
-  // void _checkIfDarkModeEnabled() {
-  //   final ThemeData theme = Theme.of(context);
-  //   theme.brightness == ThemeData().brightness
-  //       ? _darkModeEnabled = true
-  //       : _darkModeEnabled = false;
-  // }
+  bool _checkIfDarkModeEnabled() {
+    final ThemeData theme = Theme.of(context);
+    theme.brightness == ThemeData().brightness
+        ? _darkModeEnabled = true
+        : _darkModeEnabled = false;
+    return _darkModeEnabled;
+  }
 
   // bool dark = false;
   @override
   Widget build(BuildContext context) {
+    bool response = _checkIfDarkModeEnabled();
+    print(response);
+    bool check = response;
+    print(response);
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            if (light == false)
-              Text("Dark Mode Disabled")
-            else
-              Text("Dark Mode Enabled"),
-            Switch(
-              value: light,
-              onChanged: (value) {
-                setState(() {
-                  light = value;
-                  if (light) {
-                    print("Dark Mode");
-                    MyApp.of(context)?.changeTheme(ThemeMode.dark);
-                  } else {
-                    print("Light Mode");
-                    MyApp.of(context)?.changeTheme(ThemeMode.light);
-                  }
-                });
-              },
-            ),
-
-            
-              ElevatedButton(
-                onPressed: () => generateRoute(context),
-                child: const Text(
-                  'Generate Password',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 17,
+      body: (response == true)
+          ? Container(
+              child: Column(
+                children: [
+                  if (check == true) Text("Dark Mode Disabled"),
+                  if (check == false) Text("Dark Mode Enabled"),
+                  Switch(
+                    value: check,
+                    onChanged: (value) {
+                      setState(() {
+                        check = value;
+                        if (light == true) {
+                          print("Dark Mode");
+                          MyApp.of(context)?.changeTheme(ThemeMode.dark);
+                        }
+                        if (light == false) {
+                          print("Light Mode");
+                          MyApp.of(context)?.changeTheme(ThemeMode.light);
+                        }
+                      });
+                    },
                   ),
-                ),
+                  ElevatedButton(
+                    onPressed: () => generateRoute(context),
+                    child: const Text(
+                      'Generate Password',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-          ],
-        ),
-      ),
+            )
+          : Container(
+              child: Column(
+                children: [
+                  if (check == true) Text("Dark Mode Disabled"),
+                  if (check == false) Text("Dark Mode Enabled"),
+                  Switch(
+                    value: check,
+                    onChanged: (value) {
+                      setState(() {
+                        check = value;
+                        if (light == false) {
+                          print("Dark Mode");
+                          MyApp.of(context)?.changeTheme(ThemeMode.dark);
+                        }
+                        if (light == true) {
+                          print("Light Mode");
+                          MyApp.of(context)?.changeTheme(ThemeMode.light);
+                        }
+                      });
+                    },
+                  ),
+                  ElevatedButton(
+                    onPressed: () => generateRoute(context),
+                    child: const Text(
+                      'Generate Password',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
