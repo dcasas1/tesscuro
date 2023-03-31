35223ce70a56c6ebf88985f063bcdc8c1c786a67
David Casas <casas181@yahoo.com>
2023-03-31 02:03:41 -0700

    Added popup menu button for accounts

--- lib/widgets/account_item.dart
+++ lib/widgets/account_item.dart
@@ -5,6 +5,13 @@ import '../providers/credentials.dart';
 import '../providers/user_credentials_struct.dart';
 import '../screens/editsettings.dart';
 
+enum AccountOptions {
+  passVisible,
+  passInvisible,
+  editAccount,
+  deleteAccount,
+}
+
 class AccountView extends StatefulWidget {
   const AccountView({super.key});
 
@@ -18,16 +25,33 @@ class _AccountViewState extends State<AccountView> {
     Navigator.of(ctx).pushNamed(EditSettings.routeName);
   }
 
+  Future<void> _delete(BuildContext context) async {
+    final account = Provider.of<Accounts>(context, listen: false);
+    final scaffold = ScaffoldMessenger.of(context);
+    try {
+      await Provider.of<Credentials>(context, listen: false)
+          .deleteAccount(account.id);
+    } catch (error) {
+      scaffold.showSnackBar(
+        const SnackBar(
+          content: Text(
+            'Failed to delete Account',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      );
+    }
+  }
+
   @override
   void initState() {
-    _passwordVisible = true;
+    _passwordVisible = false;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     final account = Provider.of<Accounts>(context, listen: false);
-    final scaffold = ScaffoldMessenger.of(context);
     return Dismissible(
       key: ValueKey(account.siteUrl),
       direction: DismissDirection.endToStart,
@@ -74,39 +98,82 @@ class _AccountViewState extends State<AccountView> {
           size: 40,
         ),
       ),
-      onDismissed: (direction) async {
-        try {
-          await Provider.of<Credentials>(context, listen: false)
-              .deleteAccount(account.id);
-        } catch (error) {
-          scaffold.showSnackBar(
-            const SnackBar(
-              content: Text(
-                'Failed to delete Account',
-                textAlign: TextAlign.center,
-              ),
-            ),
-          );
-        }
+      onDismissed: (direction) {
+        _delete(context);
       },
       child: ListTile(
         title: Text(account.siteName),
         subtitle: _passwordVisible
             ? Text(
-                "Username: ${account.userName}\nPassword: ${account.password.replaceAll(account.password, "********")}")
+                "Username: ${account.userName}\nPassword: ${account.password}")
             : Text(
-                "Username: ${account.userName}\nPassword: ${account.password}"),
+                "Username: ${account.userName}\nPassword: ${account.password.replaceAll(account.password, "********")}"),
         trailing: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            IconButton(
-              icon: Icon(
-                  _passwordVisible ? Icons.visibility_off : Icons.visibility),
-              onPressed: () {
+            PopupMenuButton(
+              onSelected: (AccountOptions selection) {
                 setState(() {
-                  _passwordVisible = !_passwordVisible;
+                  if (selection == AccountOptions.passVisible) {
+                    _passwordVisible = true;
+                  } else if (selection == AccountOptions.passInvisible) {
+                    _passwordVisible = false;
+                  } else if (selection == AccountOptions.editAccount) {
+                  } else {
+                    showDialog(
+                      context: context,
+                      builder: (ctx) => AlertDialog(
+                        title: const Text(
+                          'Are you sure?',
+                        ),
+                        content: const Text(
+                          'Do you want to remove this account?',
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(ctx).pop(false);
+                            },
+                            child: const Text('No'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(ctx).pop();
+                              _delete(context);
+                            },
+                            child: const Text('Yes'),
+                          )
+                        ],
+                      ),
+                    );
+                  }
                 });
               },
+              itemBuilder: (BuildContext context) =>
+                  <PopupMenuEntry<AccountOptions>>[
+                !_passwordVisible
+                    ? const PopupMenuItem(
+                        value: AccountOptions.passVisible,
+                        child: Text('Show Password'),
+                      )
+                    : const PopupMenuItem(
+                        value: AccountOptions.passInvisible,
+                        child: Text('Hide Password'),
+                      ),
+                const PopupMenuDivider(),
+                const PopupMenuItem(
+                  value: AccountOptions.editAccount,
+                  child: Text('Edit Account'),
+                ),
+                const PopupMenuDivider(),
+                const PopupMenuItem(
+                  value: AccountOptions.deleteAccount,
+                  child: Text('Delete Account', style: TextStyle(color: Colors.red),),
+                ),
+              ],
+              icon: const Icon(
+                Icons.more_vert,
+              ),
             ),
           ],
         ),
