70b7aa298ae9f25334a319097828426d075157d5
David Casas <dcasas1@csub.edu>
2023-04-14 15:14:41 -0700

    Cleaned up code and resolved warnings

--- lib/screens/addcredentials.dart
+++ lib/screens/addcredentials.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:encrypt/encrypt.dart' as enc;
 import './generator.dart';
-import './nav_bar.dart';
 
 class AddCredentials extends StatefulWidget {
   const AddCredentials({super.key});
