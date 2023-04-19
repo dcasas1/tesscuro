daa07adfca21b60c80bd730715d975de5e73ab83
David Casas <casas181@yahoo.com>
2023-04-18 22:51:26 -0700

    Added Auto-Logout if app in background

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -31,7 +33,39 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  Timer? _backTimer;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      if (_backTimer != null) {
+        _backTimer!.cancel();
+      }
+      _backTimer = Timer(const Duration(minutes: 15), () {
+        FirebaseAuth.instance.signOut();
+      });
+    }
+    if (state == AppLifecycleState.resumed) {
+      if (_backTimer != null) {
+        _backTimer!.cancel();
+      }
+    }
+    super.didChangeAppLifecycleState(state);
+  }
+
   @override
   Widget build(BuildContext context) {
     return AdaptiveTheme(
